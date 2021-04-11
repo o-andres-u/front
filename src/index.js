10c4fda9ffbc1d351cdef0dc85bf18de1c4527e6
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-04-10 21:59:12 -0500

    Adding front

--- src/index.js
+++ src/index.js
@@ -1,17 +1,119 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit() {
+        this.setState({isSubmitted: true})
+    }
+
+    render() {
+        return (
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                  <input type="number" value={this.state.value} onChange={this.handleChange} placeholder="Employee ID "/>
+                  <input type="submit" value="Get Employees" />
+              </form>
+              {this.state.isSubmitted && <Table />}
+          </div>
+        );
+    }
+}
+
+class Table extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            employees: []
+        };
+
+        this.getHeader = this.getHeader.bind(this);
+        this.getRowsData = this.getRowsData.bind(this);
+    }
+
+    getHeader() {
+
+    }
+
+    getRowsData() {
+
+    }
+
+    componentDidMount () {
+        if (this.state.value) {
+            axios.get(`http://localhost:8080/employees/${this.state.value}`)
+              .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    })
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                }
+              )
+        } else {
+            axios.get('http://localhost:8080/employees')
+              .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        employees: result.employees
+                    })
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                }
+              )
+        }
+    }
+
+    render() {
+        const { error, isLoaded } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return(
+              <div>
+                  <table>
+                      <thead>
+                        <tr>{ this.getHeader() }</tr>
+                      </thead>
+                      <tbody>
+                        { this.getRowsData() }
+                      </tbody>
+                  </table>
+              </div>
+            );
+        }
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Home />,
+    document.getElementById('root')
+)
