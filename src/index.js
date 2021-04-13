0ae243e18efe8fa8a3454d574d221d05f4e42ff5
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-04-12 20:31:42 -0500

    Adding components and styling

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import './index.css'
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
-
+        this.state = {
+            value: '',
+            isSubmitted: false
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -16,18 +18,20 @@ class Home extends React.Component {
         this.setState({value: event.target.value});
     }
 
-    handleSubmit() {
-        this.setState({isSubmitted: true})
+    handleSubmit(event) {
+        this.setState({isSubmitted: true});
     }
 
     render() {
         return (
           <div>
-              <form onSubmit={this.handleSubmit}>
-                  <input type="number" value={this.state.value} onChange={this.handleChange} placeholder="Employee ID "/>
-                  <input type="submit" value="Get Employees" />
-              </form>
-              {this.state.isSubmitted && <Table />}
+              {!this.state.isSubmitted && (
+                <form onSubmit={this.handleSubmit}>
+                    <input type="number" value={this.state.value} onChange={this.handleChange} placeholder="Employee ID "/>
+                    <input type="submit" value="Get Employees" />
+                </form>
+              )}
+              {this.state.isSubmitted && <Table value={this.state.value}/>}
           </div>
         );
     }
@@ -47,21 +51,40 @@ class Table extends React.Component {
     }
 
     getHeader() {
-
+        if (this.state.employees[0]) {
+            let header = Object.keys(this.state.employees[0]);
+            return header.map((key, index) => {
+                return <th key={index}>{key.toUpperCase()}</th>
+            });
+        }
     }
 
     getRowsData() {
-
+        return this.state.employees.map(employee => {
+            const { id, name, contractType, roleName, annualSalary } = employee
+            return (
+              <tr key={id}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{contractType}</td>
+                  <td>{roleName}</td>
+                  <td>{annualSalary}</td>
+              </tr>
+            )
+        });
     }
 
     componentDidMount () {
-        if (this.state.value) {
-            axios.get(`http://localhost:8080/employees/${this.state.value}`)
+        this.setState({
+            isLoaded: true
+        });
+        if (this.props.value) {
+            axios.get(`http://localhost:8080/employees/${this.props.value}`)
               .then(
                 (result) => {
                     this.setState({
                         isLoaded: true,
-                        items: result
+                        employees: [result.data]
                     })
                 },
                 (error) => {
@@ -77,7 +100,7 @@ class Table extends React.Component {
                 (result) => {
                     this.setState({
                         isLoaded: true,
-                        employees: result.employees
+                        employees: result.data
                     })
                 },
                 (error) => {
@@ -99,12 +122,10 @@ class Table extends React.Component {
         } else {
             return(
               <div>
-                  <table>
-                      <thead>
-                        <tr>{ this.getHeader() }</tr>
-                      </thead>
+                  <table id='employees'>
                       <tbody>
-                        { this.getRowsData() }
+                          <tr>{ this.getHeader() }</tr>
+                          { this.getRowsData() }
                       </tbody>
                   </table>
               </div>
